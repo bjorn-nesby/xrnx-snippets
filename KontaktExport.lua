d5884653ea2235e3a1f9a1fa8bf371f0f03e9001
bjorn-nesby <bjorn.nesby@gmail.com>
2016-09-24 01:05:14 +0200

    Remove comma, replace spaces

--- KontaktExport.lua
+++ KontaktExport.lua
@@ -2,18 +2,23 @@
 Format filenames for export to Kontakt sampler
 Removes dash and convert velocity to decimal
 
-Input: e.g. VST: Rhode (Default)_0x2A_C-5
-Output e.g. VST: Rhode (Default)_42_C5
+Input: e.g. VST: iblit (PwmBass 1)_0x7F_C-3
+Output e.g. VST_iblit_PwmBass_1_127_C3
 
 ]]
 
 local output_folder = "c:/temp/" -- folder needs to exist
 local instr = renoise.song().selected_instrument
 for _,sample in ipairs(instr.samples) do
-  local filename = output_folder .. sample.name
+  local filename = sample.name
   filename = string.gsub(filename,"(.*)_(0x%x%x)_([A-Z])-?(#?%d)",function(a,b,c,d)
+    a = string.gsub(a,"VST:","VST")
+    a = string.gsub(a,"AU:","AU")
+    a = string.gsub(a,"[%(%)]","")
+    a = string.gsub(a,"%s","_")
     return a.."_"..("%d"):format(tonumber(b)).."_"..c..d
   end)
+  filename = output_folder .. filename
   print(filename)
   --renoise.app():save_instrument_sample(filename)
 end
